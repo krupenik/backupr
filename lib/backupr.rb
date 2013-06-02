4f3e35dca6ab6621c9a8f55f22ddc79c98ab95be
Dennis Krupenik <dennis@krupenik.com>
2013-06-03 01:29:05 +0300

    wip on host backup routine

--- lib/backupr.rb
+++ lib/backupr.rb
@@ -1,12 +1,79 @@
 require "backupr/version"
 require "backupr/cleaner"
+require "backupr/host"
+require "backupr/actor"
 
 module Backupr
   DirFormat = "%F-%H%M%S"
-  
+  StateFilename = "backupr.state"
+  ProgressMark = "inProgress"
+  Commands = %w(backup).freeze
+
+  Error = Class.new StandardError
+  DuplicateEntryError = Class.new Error
+
+  class Config
+    attr_accessor :root
+  end
+
+  @hosts = {}
+  @config = Config.new
+
   class << self
-    def host(name, &block)
-      Backupr::Host.new(name, &block)
+    # we don't simply give away the storage
+    def hosts
+      @hosts.dup
+    end
+
+    def config
+      @config.dup
+    end
+
+    def clear
+      @hosts.clear
+    end
+
+    def configure
+      yield @config if block_given?
+    end
+
+    def host address, &block
+      self.add_host(Backupr::Host.new(address, &block))
+    end
+
+    def add_host host
+      # we raise an exception instead of silently replacing to keep config files clean
+      raise Backupr::DuplicateEntryError if @hosts.has_key?(host.address)
+
+      @hosts[host.address] = host
+    end
+
+    def backup
+      @hosts.each { |_, h| h.backup }
+    end
+
+    def load_config(config)
+      files = File.directory?(config) ? Dir['**/*.conf'] : Dir[config]
+      abort "No files could be found (search path: #{config})" if files.empty?
+      files.each do |f|
+        unless load_file(f)
+          abort "File '#{f}' could not be loaded"
+        end
+      end
+    end
+
+    def load_file(f)
+      load File.expand_path(f)
+      true
+    rescue Exception => e
+      if e.instance_of?(SystemExit)
+        raise
+      else
+        puts "There was an error in #{f}"
+        puts "\t" + e.message
+        puts "\t" + e.backtrace.join("\n\t")
+        false
+      end
     end
   end
 end
--- lib/backupr/host.rb
+++ lib/backupr/host.rb
@@ -1,4 +1,37 @@
+require 'fileutils'
+
 module Backupr
   class Host
+    attr_accessor :address, :storage, :sqldump
+
+    def initialize address, &block
+      @address = address
+      yield self if block_given?
+    end
+
+    def backup
+      complete = []
+
+      backup_root = File.join(Backupr.config.root, self.address, Time.now.strftime(Backupr::DirFormat))
+      backup_temp = "#{backup_root}.#{Backupr::ProgressMark}"
+
+      if self.storage
+        complete << Backupr::Actor.create(:storage, self.address, backup_temp, self.storage).act
+      end
+
+      if self.sqldump
+        complete << Backupr::Actor.create(:sqldump, self.address, backup_temp, self.sqldump).act
+      end
+
+      if complete.any?
+        latest_path = File.expand_path(File.join("..", "Latest"), backup_root)
+
+        FileUtils.mv backup_temp, backup_root
+        FileUtils.rm_f latest_path
+        FileUtils.ln_sf File.basename(backup_root), latest_path
+      else
+        FileUtils.rm_rf backup_temp
+      end
+    end
   end
 end
