26db5a33dd6f423d515b2c31e80f2a595cd81238
Dennis Krupenik <dennis@krupenik.com>
2016-08-26 13:01:38 +0300

    Cleaner cleanup ^_^

--- lib/backupr/cleaner.rb
+++ lib/backupr/cleaner.rb
@@ -1,20 +1,21 @@
 require "time"
 
 module Backupr
-  class Cleaner
+  module Cleaner
     Cutoffs = [
+      [3600, "%F-%H%M"],  # minutely
       [86400, "%F-%H"],   # hourly
-      [31 * 86400, "%F"], # daily
+      [30 * 86400, "%F"], # daily
       [nil, "%Y-W%V"],    # weekly
     ]
-    
-    def self.clean(path)
+
+    def clean(path)
       now = Time.now.utc
       Dir.chdir path
 
       data = Dir["*"].map { |i| Time.strptime(i, Backupr::DirFormat).utc }
 
-      survivors = data.select { |i| i > now - Cutoffs[0][0] }      
+      survivors = data.select { |i| i > now - Cutoffs[0][0] }
       Cutoffs[0..-2].each do |interval, format|
         survivors += last_of_group(data.select { |i| i >= now - interval }) { |i| i.strftime(format) }
       end
@@ -23,9 +24,16 @@ module Backupr
 
       (data - survivors).uniq.each { |i| FileUtils.rmtree(i.strftime(Backupr::DirFormat)) }
     end
-    
-    def self.last_of_group(data, &block)
-      data.group_by(&block).map { |k, v| v.last }
+
+    def survivors(data, from, cutoffs)
+      survivors = cutoffs.reduce([]) { |a, (interval, format)|
+        a << data.select { |i| i >= from - interval }
+        a
+      }
+    end
+
+    def last_of_group(data, &block)
+      data.group_by(&block).values.map(&:last)
     end
   end
 end
